b5bfea379d6dce39641c76c683408a00a8274b8b
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2019-08-21 11:25:33 -0300

    Bug corregido, se agrego notificacion.

--- src/app/Index.js
+++ src/app/Index.js
@@ -1,14 +1,53 @@
 
+ import style from "../main.css"
 
-const { Productos } = require("./Productos")
-const { Vista }=require("./Vista")
+ class Productos{
+    constructor(){
+        this.productos=["Manzanas","Peras","Bananas"]
+        this.subcriptores=[]
+    }
+    agregarProductos(producto){
+        this.productos.push(producto)
+        this.notificarSubcriptores()
+    }
+    agregarSubcriptores(callback){
+        this.subcriptores.push(callback)
+    }
+    notificarSubcriptores(){
+        this.subcriptores.forEach((subcriptor)=>{
+            subcriptor(this.productos)
+        })
+    }
+}
+
+
+
+ class Vista{
+	constructor(){
+			productos.agregarSubcriptores(this.render)
+	}
+	render(productos){
+		document.querySelector(".ul").innerHTML= productos.map((producto)=>`<li>${producto}</li>`).join("")
+	}
+}
+
 
 document.querySelector(".button").addEventListener("click",(e)=>{
     e.preventDefault()
     const input=document.querySelector(".input")
     const producto=input.value
-    productos.agregarProducto(producto)
+    productos.agregarProductos(producto)
 })
+const productos= new Productos()
+const vista= new Vista()
 
-new Productos()
-new Vista()
+productos.agregarSubcriptores((productos)=>{
+const producto=productos[productos.length-1]
+document.querySelector(".message").innerHTML= `<span class="alert">${producto}</span>`
+
+setTimeout(function () {
+            document.querySelector('.message').remove()
+        }, 1000)
+
+
+})
--- src/app/Vista.js
+++
@@ -1,9 +0,0 @@
-export class Vista{
-	constructor(){
-			productos.agregarSubcriptores(this.render)
-	}
-	render(productos){
-		document.querySelector(".ul").innerHTML=productos.map((producto)=>`<li>${producto}</li>`).join("")
-	}
-}
-
